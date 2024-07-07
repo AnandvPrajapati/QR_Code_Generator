33765255c436ff339182b6e88f53f16ee4f66e32
AnandvPrajapati <159772603+AnandvPrajapati@users.noreply.github.com>
2024-07-07 22:55:09 +0530

    Updated

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ inquirer
         const url = answers.URL;
         var qr_svg = qr.image(url);
         qr_svg.pipe(fs.createWriteStream('qr-img.png'));
+        fs.writeFile("message.txt", "Hello Node", (err) => {
+          if (err) throw err;
+          console.log("The file has been saved!");
+        });
     })
     .catch((error) => {
         if (error.isTtyError) {
